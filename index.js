75d3007dcc6dd5d4190cd49a3f3e184116c4cf8c
Rudolf Ádám <sari.vogel.94@gmail.com>
2018-12-13 21:00:47 +0100

    Wite ip address of requester in colors of rainbow

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const http = require('http');
+var colors = require('colors');
 
 http.createServer(function (req, res){
     fs.readFile('index.html', function(err, data) {
-        console.log(req.connection.remoteAddress);
+        console.log(req.connection.remoteAddress.rainbow);
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
