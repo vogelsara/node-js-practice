69c7e28a98430c161eb356bb0e08eece3644b85e
Rudolf Ádám <sari.vogel.94@gmail.com>
2018-12-13 20:54:24 +0100

    Write request ip address to console

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 
 http.createServer(function (req, res){
     fs.readFile('index.html', function(err, data) {
+        console.log(req.connection.remoteAddress);
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
