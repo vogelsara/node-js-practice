89a9bd48d401d1c9bffb51cc912027992de46f07
Rudolf Ádám <sari.vogel.94@gmail.com>
2018-12-13 20:42:06 +0100

    Server responds by sending index.html

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const http = require('http');
 
 http.createServer(function (req, res){
-    res.write('Hello world!');
-    res.end();
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 }).listen(8080);
